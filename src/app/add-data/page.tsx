2e6d0de7cdefa5fdf8ff60b71014f8212d79d050
Armaan <armaan33000@gmail.com>
2024-08-01 21:50:23 +0530

    fix

---
+++ src/app/add-data/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./adddatapage.css";
+
+type Props = {};
+
+const AddDataPage = (props: Props) => {
+  return (
+    // TODO: FIX THIS , YAEY
+    <div>AddDataPage</div>
+  );
+};
+
+export default AddDataPage;
+    
